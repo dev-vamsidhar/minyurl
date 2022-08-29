f371bcf4e929caf1d630cf2764ffdbde83a33601
Vamsidhar <vamsubala1@gmail.com>
2022-08-29 17:57:38 +0530

    added cors

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+var cors = require("cors");
 require("./databse/dbconfig");
 require("dotenv").config();
 
 const app = express();
 //Middleware to handle the json data
 app.use(express.json());
+app.use(cors());
 
-var router = require("./routes/userroutes")
-app.use(router)
-
+var router = require("./routes/userroutes");
+app.use(router);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started at ${process.env.PORT}`);
