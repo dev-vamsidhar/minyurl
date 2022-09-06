822322ffc930446303ce032f2820e5bd39e9da28
Vamsidhar <vamsubala1@gmail.com>
2022-09-06 22:48:14 +0530

    Cors test1

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ require("dotenv").config();
 const app = express();
 // //Middleware to handle the json data
 app.use(express.json());
-app.use(cors());
+app.use(cors(), (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  next();
+});
 
 var router = require("./routes/userroutes");
 app.use(router);
